d0064753ba6500cab618c1f6f8870c7424edfd71
Steinaver <astral.divinity.19@gmail.com>
2025-06-17 21:27:50 +0300

    theme-switcher, button ui kit, svg-loader, file-loader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,16 @@
 import { AppRouter } from "app/providers/router";
 import { useTheme } from "app/providers/ThemeProvider";
-import { Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames/classNames";
-import './styles/index.scss';
 import { Navbar } from "widgets/Navbar";
+import './styles/index.scss';
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme();
+    const { theme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar />
             <AppRouter />
-            <button onClick={toggleTheme}>TOGGLE</button>
         </div>
     )
 };
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,7 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
-import classes from './Navbar.module.scss';
+import classes from "./Navbar.module.scss";
+import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 
 interface NavbarProps {
     className?: string;
@@ -9,6 +10,7 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
     return (
         <div className={classNames(classes.Navbar, {}, [className])}>
+            <ThemeSwitcher />
             <div className={classes.links}>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={'/'}>
                     Главная
