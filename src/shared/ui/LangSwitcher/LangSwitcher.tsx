232d13b1c647cab75f6fe10e1b5f28550ef832f7
Steinaver <astral.divinity.19@gmail.com>
2025-06-18 21:55:55 +0300

    i18n & define plugin webpack

---
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -0,0 +1,26 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import classes from "./LangSwitcher.module.scss";
+import { Button, ThemeButton } from "../Button/Button";
+import { useTranslation } from "react-i18next";
+
+interface LangSwitcherProps {
+    className?: string;
+}
+
+export const LangSwitcher = ({ className }: LangSwitcherProps) => {
+    const { t, i18n } = useTranslation("translation");
+
+    const toggle = () => {
+        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
+    }
+
+    return (
+        <Button
+            className={classNames(classes.LangSwitcher, {}, [className])}
+            theme={ThemeButton.CLEAR}
+            onClick={toggle}
+        >
+            {t('language')}
+        </Button>
+    )
+};
