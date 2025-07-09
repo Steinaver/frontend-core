e1a8134925dbc58d4045811fcda126a4a021a40a
Steinaver <astral.divinity.19@gmail.com>
2025-07-09 14:22:06 +0300

    add ci github-actions

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
 import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 import classes from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -11,7 +12,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
-
+    const { t } = useTranslation('translation');
     const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
@@ -25,7 +26,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 data-testid="sidebar-toggle"
                 onClick={onToggle}
             >
-                toggle
+                {t('toggle')}
             </Button>
             <div className={classes.switchers}>
                 <ThemeSwitcher />
