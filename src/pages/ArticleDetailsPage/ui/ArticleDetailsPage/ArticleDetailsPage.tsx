1527d4d0f1aa4f622cea00d99e87a62083fb7633
Steinaver <astral.divinity.19@gmail.com>
2025-08-22 21:01:55 +0300

    add articles page & article details page

---
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -0,0 +1,24 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { memo } from 'react';
+import classes from './ArticleDetailsPage.module.scss';
+
+interface ArticleDetailsPageProps {
+    className?: string;
+}
+
+const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
+    const {
+        className,
+    } = props;
+
+    const { t } = useTranslation('article');
+
+    return (
+        <div className={classNames(classes.ArticleDetailsPage, {}, [className])}>
+            Article Details Page
+        </div>
+    );
+};
+
+export default memo(ArticleDetailsPage);
---
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -0,0 +1,24 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { memo } from 'react';
+import classes from './ArticlesPage.module.scss';
+
+interface ArticlesPageProps {
+    className?: string;
+}
+
+const ArticlesPage = (props: ArticlesPageProps) => {
+    const {
+        className,
+    } = props;
+
+    const { t } = useTranslation('translation');
+
+    return (
+        <div className={classNames(classes.ArticlesPage, {}, [className])}>
+            Articles Page
+        </div>
+    );
+};
+
+export default memo(ArticlesPage);
