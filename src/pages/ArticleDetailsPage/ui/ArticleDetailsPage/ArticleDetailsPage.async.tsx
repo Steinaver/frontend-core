93bebcf5a7fc2c7da8309ffe21e864f7ad3ad7f5
Steinaver <astral.divinity.19@gmail.com>
2025-09-13 15:40:14 +0300

    add fetching articles & view type selector

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.async.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.async.tsx
@@ -3,5 +3,5 @@ import { lazy } from 'react';
 export const ArticleDetailsPageAsync = lazy(() => new Promise((resolve) => {
     // @ts-ignore
     // test loading
-    setTimeout(() => resolve(import('./ArticleDetailsPage')), 1500);
+    setTimeout(() => resolve(import('./ArticleDetailsPage')), 400);
 }));
--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,12 +1,25 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import { memo } from 'react';
-import { ArticleList, ArticleView } from 'entities/Article';
+import { memo, useCallback } from 'react';
+import { ArticleList, ArticleView, ArticleViewSelector } from 'entities/Article';
+import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useSelector } from 'react-redux';
+import { fetchArticlesList } from '../../model/services/fetchArticlesList/fetchArticlesList';
+import { getArticlesPageIsLoading } from '../../model/selectors/getArticlesPageIsLoading';
+import { getArticlesPageError } from '../../model/selectors/getArticlesPageError';
+import { getArticlesPageView } from '../../model/selectors/getArticlesPageView';
+import { articlesPageActions, articlesPageReducer, getArticles } from '../../model/slices/articlesPageSlice';
 import classes from './ArticlesPage.module.scss';
 
 interface ArticlesPageProps {
     className?: string;
 }
+
+const reducers: ReducersList = {
+    articlesPage: articlesPageReducer,
+};
 
 const ArticlesPage = (props: ArticlesPageProps) => {
     const {
@@ -14,14 +27,32 @@ const ArticlesPage = (props: ArticlesPageProps) => {
     } = props;
 
     const { t } = useTranslation('translation');
+    const dispatch = useAppDispatch();
+    const articles = useSelector(getArticles.selectAll);
+    const isLoading = useSelector(getArticlesPageIsLoading);
+    const error = useSelector(getArticlesPageError);
+    const view = useSelector(getArticlesPageView);
+
+    const onChangeView = useCallback((view: ArticleView) => {
+        dispatch(articlesPageActions.setView(view));
+    }, [dispatch]);
+
+    useInitialEffect(() => {
+        dispatch(fetchArticlesList());
+        dispatch(articlesPageActions.initState());
+    });
 
     return (
-        <div className={classNames(classes.ArticlesPage, {}, [className])}>
-            <ArticleList
-                view={ArticleView.BIG}
-                articles={[]}
-            />
-        </div>
+        <DynamicModuleLoader reducers={reducers}>
+            <div className={classNames(classes.ArticlesPage, {}, [className])}>
+                <ArticleViewSelector view={view} onViewClick={onChangeView} />
+                <ArticleList
+                    isLoading={isLoading}
+                    view={view}
+                    articles={articles}
+                />
+            </div>
+        </DynamicModuleLoader>
     );
 };
 
