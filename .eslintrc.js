4a1bb55c281d6bfb2f3840c5d1e64930082677b1
Steinaver <astral.divinity.19@gmail.com>
2025-10-27 18:54:54 +0200

    add linter rule for layer imports, fix errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -82,6 +82,13 @@ module.exports = {
                 testFilesPatterns: ['**/*.test.*', '**/*.story.*', '**/StoreDecorator.tsx'],
             },
         ],
+        'react-project-plugin/layer-imports': [
+            'error',
+            {
+                alias: '@',
+                ignoreImportPatterns: ['**/StoreProvider', '**/testing'],
+            },
+        ],
     },
     globals: {
         __IS_DEV__: true,
--- src/shared/types/router.ts
+++ src/shared/types/router.ts
@@ -1,4 +1,5 @@
 import { RouteProps } from 'react-router-dom';
+// eslint-disable-next-line react-project-plugin/layer-imports
 import { UserRole } from '@/entities/User';
 
 export type AppRoutesProps = RouteProps & {
